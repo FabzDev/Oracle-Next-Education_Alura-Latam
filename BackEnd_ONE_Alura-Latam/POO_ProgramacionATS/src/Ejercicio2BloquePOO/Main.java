e9967a5878c9296c0c2e76d9ec0f55f2b420d22b
fabztech <fabztech@outlook.com>
2023-02-27 01:25:34 -0500

    POO_ProgramacionATS - Ejercicio 2 - completo

--- BackEnd_ONE_Alura-Latam/POO_ProgramacionATS/src/Ejercicio2BloquePOO/Main.java
+++ BackEnd_ONE_Alura-Latam/POO_ProgramacionATS/src/Ejercicio2BloquePOO/Main.java
@@ -1,16 +1,25 @@
 package Ejercicio2BloquePOO;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Tablero pizarra1 = new Tablero(100,100);
-		
-		pizarra1.moverArriba();
-		pizarra1.moverIzquierda();
-		pizarra1.moverDerecha();
-		pizarra1.moverAbajo();
-		pizarra1.moverAbajo();
-		pizarra1.moverDerecha();
-			
+		while (pizarra1.x < 150 && pizarra1.y<150) {
+			String movimiento = JOptionPane.showInputDialog("Ingresa la direccion de movimiento");
+			if (movimiento.contentEquals("arriba")) {
+				pizarra1.moverArriba();
+			} else if (movimiento.contentEquals("abajo")) {
+			pizarra1.moverAbajo();
+			} else if (movimiento.contentEquals("derecha")) {
+			pizarra1.moverDerecha();
+			} else if (movimiento.contentEquals("izquierda")) {
+			pizarra1.moverIzquierda();
+			} else {
+			System.out.println("Movimiento invalido");
+			}
+		}
 	}
 }
+
