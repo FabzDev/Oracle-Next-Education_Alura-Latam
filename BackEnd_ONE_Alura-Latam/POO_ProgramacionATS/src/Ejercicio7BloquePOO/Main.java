749696daa333c7579cd8865e2162ac766de2ff29
fabztech <fabztech@outlook.com>
2023-03-03 02:00:05 -0500

    POO_ProgramacionATS - Ejercicio 7 - completo v2.0

--- BackEnd_ONE_Alura-Latam/POO_ProgramacionATS/src/Ejercicio7BloquePOO/Main.java
+++ BackEnd_ONE_Alura-Latam/POO_ProgramacionATS/src/Ejercicio7BloquePOO/Main.java
@@ -3,6 +3,24 @@ package Ejercicio7BloquePOO;
 import java.util.Scanner;
 
 public class Main {
+	
+	public static int buscador(Cuenta[] cuenta, int nc) {
+		int index = 0;
+		boolean encontrado = false;
+		for (int i = 0; i < cuenta.length; i++) {
+			if(cuenta[i].getNumeroCuenta() == nc) {
+				index = i;
+				encontrado = true;
+			}
+		}
+		if(encontrado) {
+			return index;
+		} else {
+			return -1;
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		
@@ -43,10 +61,8 @@ public class Main {
 		int opcion;
 		
 		do {
-			System.out.println("\nSeleccione la cuenta que desea utilizar");
-			int n = entrada.nextInt()-1;
 			
-			System.out.println("\nSeleccione el tipo de operacion que desea realizar: ");
+			System.out.println("\n\nSeleccione el tipo de operacion que desea realizar: ");
 			System.out.println("1. Obtener saldo");
 			System.out.println("2. Ingresar dinero");
 			System.out.println("3. Retirar dinero");
@@ -55,21 +71,42 @@ public class Main {
 			
 			switch (opcion) {
 			
-			case 1: System.out.println("El saldo de la cuenta seleccionada es: " + cuentas[n].getSaldo());
-			break;
+			case 1: 
+				System.out.println("\nIngrese el numero de la cuenta");
+				int nC = entrada.nextInt();
+				
+				if(buscador(cuentas,nC) == -1) {
+					System.out.println("El numero de la cuenta esta errado.");
+				}else {
+					System.out.println("\nEl saldo de la cuenta seleccionada es: " + cuentas[buscador(cuentas,nC)].getSaldo());					
+				}
+				
+				break;
 			
 			case 2: 
-				System.out.println("Ingrese la candidad que desea depositar:");
+				System.out.println("\nIngrese el numero de la cuenta");
+				nC = entrada.nextInt();
+				
+				System.out.println("\nConfirme la candidad de dinero que desea depositar:");
 				double deposito = entrada.nextDouble();
-				cuentas[n].ingresarDinero(deposito);
+				
+				cuentas[buscador(cuentas,nC)].ingresarDinero(deposito);
 				System.out.println("Deposito exitoso");
+				System.out.println("\nSu nuevo saldo es: " + cuentas[buscador(cuentas,nC)].getSaldo());
+				
 			break;
 			
 			case 3: 
-				System.out.println("Ingrese la candidad que desea retirar:");
+				System.out.println("\nIngrese el numero de la cuenta");
+				nC = entrada.nextInt();
+				
+				System.out.println("\nConfirme la candidad de dinero que desea retirar:");
 				double retiro = entrada.nextDouble();
-				cuentas[n].retirarDinero(retiro);
+				
+				cuentas[buscador(cuentas,nC)].retirarDinero(retiro);
 				System.out.println("Retiro exitoso");
+				System.out.println("\nSu nuevo saldo es: " + cuentas[buscador(cuentas,nC)].getSaldo());
+				
 			break;
 			
 			}
